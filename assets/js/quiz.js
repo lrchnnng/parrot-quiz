569c14ea158b562ec73d2e992b121f1be1def794
lrchnnng <laurachanning@hotmail.co.uk>
2023-09-24 11:05:13 +0100

    added template functions to quiz.js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,4 +1,13 @@
-// Array to generate questions and answers
-const getQuestions = {
+/**
+ * getQuestion 
+ * (question generated on document load and when
+ * submit button is pressed)
+ */
 
-}
+// checkAnswer (checks if chosen answer === correct answer)
+
+// incorrectAnswer (-1 point)
+
+// correctAnswer (+1 point)
+
+// Array of questions (img url and text answers)
