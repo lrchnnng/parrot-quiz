c7871c839d79165b95f28764076d48825117e297
lrchnnng <laurachanning@hotmail.co.uk>
2023-10-03 14:57:32 +0100

    created variables to target question, buttons and score to update later

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -52,3 +52,10 @@ const questions = [
    },
 ]
 
+const questionElement = document.getElementById('question');
+const answerButton = document.getElementById('answer-buttons');
+const nextButton = document.getElementById('next-question');
+
+let currentQuestionIndex = 0;
+let score = 0;
+
