c28868ac43f46410c3f594190a0cf47a362daf1e
lrchnnng <laurachanning@hotmail.co.uk>
2023-10-03 14:53:59 +0100

    moved quiz code to test.js to restart

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,214 +1,54 @@
-$(document).ready(function() {
-    nextQuestion();
-    console.log("ready!");
-});
-
-const options = document.querySelector('.q-options');
-
 // An array of objects containing quiz images and answers
-const quizQuestions = [
-    {
-        questionUrl: 'assets/images/main-img/african.jpg',
-        a: 'African Grey',
-        b: 'Cockatiel',
-        c: 'Burrowing',
-        fact: '',
-    },
-    {
-       questionUrl: 'assets/images/main-img/goffin.jpg',
-       a: 'Goffin Cockatoo',
-       b: 'Quaker',
-       c: 'Pionus',
-       fact: '',
-    },
-    {
-        questionUrl: 'assets/images/main-img/scarlet.jpg',
-        a: 'Scarlett Macaw',
-        b: "Meyer's",
-        c: 'Yellow-Naped Amazon',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/cockatiel.jpg',
-        a: 'Cockatiel',
-        b: 'Hyacinth Macaw',
-        c: 'Sun Conure',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/eclectus.jpg',
-        a: 'Eclectus',
-        b: 'Senegal',
-        c: 'Indian Ringneck',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/amazon.jpg',
-        a: 'Yellow-Naped Amazon',
-        b: 'Kakariki',
-        c: 'Green Cheek Conure',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/pionus.jpg',
-        a: 'Pionus',
-        b: 'Caique',
-        c: 'African Grey',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/gcc.jpg',
-        a: 'Green Cheek Conure',
-        b: 'Goffin Cockatoo',
-        c: 'Galah Cockatoo',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/caique.jpg',
-        a: 'Caique',
-        b: 'Lorikeet',
-        c: 'Cockatiel',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/irn.jpg',
-        a: 'Indian Ringneck',
-        b: 'Lovebird',
-        c: 'African Grey',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/quaker.jpg',
-        a: 'Quaker',
-        b: 'Cockatiel',
-        c: 'Eclectus',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/lovebird.jpg',
-        a: 'Lovebird',
-        b: 'Scarlet Macaw',
-        c: 'Goffin Cockatoo',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/lori.jpg',
-        a: 'Lorikeet',
-        b: 'Indian Ringneck',
-        c: 'Green Cheek Conure',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/hyacinth.jpg',
-        a: 'Hyacinth Macaw',
-        b: 'Kakariki',
-        c: 'Yellow-Naped Amazon',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/galah.jpg',
-        a: 'Galah Cockatoo',
-        b: "Meyer's",
-        c: 'Quaker',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/senegal.jpg',
-        a: 'Senegal',
-        b: 'Green Cheek Conure',
-        c: 'Eclectus',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/burrowing.jpg',
-        a: 'Burrowing',
-        b: 'Senegal',
-        c: 'Lorikeet',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/meyers.jpg',
-        a: "Meyer's",
-        b: 'Burrowing',
-        c: 'Goffin Cockatoo',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/kakariki.jpg',
-        a: 'Kakariki',
-        b: 'Pionus',
-        c: 'Lovebird',
-        fact: '',
-     },
-     {
-        questionUrl: 'assets/images/main-img/sun.jpg',
-        a: 'Sun Conure',
-        b: 'Caique',
-        c: 'Cockatiel',
-        fact: '',
-     },
+const questions = [
+   {
+      question: 'assets/images/main-img/african.jpg',
+      answers: [
+         {text: "African Grey", correct: true},
+         {text: "Cockatiel", correct: false},
+         {text: "Burrowing", correct: false},
+      ],
+      fact: "According to the Guiness World Record, the oldest African Grey lived to 72 years old!",
+   },
+   {
+      question: 'assets/images/main-img/scarlet.jpg',
+      answers: [
+         {text: "Scarlet Macaw", correct: true},
+         {text: "Hyacinth Macaw", correct: false},
+         {text: "Meyer's", correct: false},
+         {text: "Yellow-Naped Amazon", correct: false},
+      ],
+      fact: "The Scarlet Macaw is the national bird of Honduras!",
+   },
+   {
+      question: 'assets/images/main-img/eclectus.jpg',
+      answers: [
+         {text: "Senegal", correct: false},
+         {text: "Indian Ringneck", correct: false},
+         {text: "Eclectus", correct: true},
+         {text: "Kakariki", correct: false},
+
+      ],
+      fact: "Male Eclectus parrots are green, females are Red!",
+   },
+   {
+      question: 'assets/images/main-img/cockatiel.jpg',
+      answers: [
+         {text: "Senegal", correct: false},
+         {text: "Cockatiel", correct: true},
+         {text: "Hyacinth Macaw", correct: false},
+         {text: "Sun Conure", correct: false},
+      ],
+      fact: "Cocktiels are better at whistling than they are at talking!",
+   },
+   {
+      question: 'assets/images/main-img/amazon.jpg',
+      answers: [
+         {text: "Sun Conure", correct: false},
+         {text: "Kakariki", correct: false},
+         {text: "Green Cheek Conure", correct: false},
+         {text: "Yellow-Naped Amazon", correct: true},
+      ],
+      fact: "Yellow-Naped Amazon parrots can live for over 50 years when cared for by humans!",
+   },
 ]
 
-
-// Targets the image element in the DOM
-const question = document.getElementById('question');
-var currentQuestionIndex = 0; // Starts the current question index at 0
-
-function nextQuestion() {
-   const currentQ = quizQuestions[currentQuestionIndex];
-
-   currentQuestionIndex++;
-
-   if (currentQuestionIndex >= quizQuestions.length) {
-   currentQuestionIndex = 0;
-   }
-   
-   question.src = currentQ.questionUrl;
-
-   // Create an array of answer options
-   var answerOptions = [currentQ.a, currentQ.b, currentQ.c];
-
-   // Shuffle the answer options
-   shuffleArray(answerOptions);
-
-   // Assign shuffled answer options to the buttons
-   document.getElementById('option1').textContent = answerOptions[0];
-   document.getElementById('option2').textContent = answerOptions[1];
-   document.getElementById('option3').textContent = answerOptions[2];
-}
-
-// Function to shuffle an array using the Fisher-Yates algorithm
-function shuffleArray(array) {
-   for (let i = array.length - 1; i > 0; i--) {
-       const j = Math.floor(Math.random() * (i + 1));
-       [array[i], array[j]] = [array[j], array[i]];
-   }
-}
-
-function checkAnswer(event, currentQ) {
-   // Get the selected answer from the event target's textContent
-   const selectedAnswer = event.target.textContent;
-
-   // Compare the selected answer with the correct answer
-   if (selectedAnswer === currentQ.a) {
-       console.log('Correct');
-   } else {//(selectedAnswer  === currentQ.b || selectedAnswer === currentQ.c) {
-       console.log('Wrong');
-   }
-}
-
-const answerButtons = document.querySelectorAll('.answer-btn');
-
-// Logs button click
-answerButtons.forEach((button) => {
-   button.addEventListener('click', (event) => {
-       console.log('Button clicked'); // Add this line for debugging
-       checkAnswer(event, quizQuestions[currentQuestionIndex]);
-   });
-});
-
-function correctAnswer(currentQ) {
-   // answerReveal.innerText = `Correct! Did you know: ${currentQ.fact}`;
-   score++; // Increase the score by 1
-   updateScore(); // Update the displayed score
-}
--- assets/js/test.js
+++ assets/js/test.js
@@ -1,24 +1,171 @@
+$(document).ready(function() {
+    nextQuestion();
+    console.log("ready!");
+});
+
+const options = document.querySelector('.q-options');
 
 
+// Targets the image element in the DOM
+const question = document.getElementById('question');
+var currentQuestionIndex = 0; // Starts the current question index at 0
 
+function nextQuestion() {
+   const currentQ = quizQuestions[currentQuestionIndex];
 
- 
+   currentQuestionIndex++;
 
+   if (currentQuestionIndex >= quizQuestions.length) {
+   currentQuestionIndex = 0;
+   }
+   
+   question.src = currentQ.questionUrl;
+
+   // Create an array of answer options
+   var answerOptions = [currentQ.a, currentQ.b, currentQ.c];
+
+   // Shuffle the answer options
+   shuffleArray(answerOptions);
+
+   // Assign shuffled answer options to the buttons
+   document.getElementById('option1').textContent = answerOptions[0];
+   document.getElementById('option2').textContent = answerOptions[1];
+   document.getElementById('option3').textContent = answerOptions[2];
+}
+
+// Function to shuffle an array using the Fisher-Yates algorithm
+function shuffleArray(array) {
+   for (let i = array.length - 1; i > 0; i--) {
+       const j = Math.floor(Math.random() * (i + 1));
+       [array[i], array[j]] = [array[j], array[i]];
+   }
+}
+
+function checkAnswer(event) {
+   // Get the selected answer from the event target's textContent
+   const selectedAnswer = event.target.textContent;
+
+   // Get the current question
+   const currentQ = quizQuestions[currentQuestionIndex - 1];
+
+   // Compare the selected answer with the correct answer
+   if (selectedAnswer === currentQ.correct) {
+       console.log('Correct');
+   } else {
+       console.log('Wrong');
+   }
+}
+
+const answerButtons = document.querySelectorAll('.answer-btn');
+
+// Logs button click
+answerButtons.forEach((button) => {
+   button.addEventListener('click', (event) => {
+       console.log('Button clicked'); // Add this line for debugging
+       checkAnswer(event, quizQuestions[currentQuestionIndex]);
+   });
+});
 
 function correctAnswer(currentQ) {
-    // answerReveal.innerText = `Correct! Did you know: ${currentQ.fact}`;
-    score++; // Increase the score by 1
-    updateScore(); // Update the displayed score
+   // answerReveal.innerText = `Correct! Did you know: ${currentQ.fact}`;
+   score++; // Increase the score by 1
+   updateScore(); // Update the displayed score
 }
 
-// incorrectAnswer (-1 point)
-function incorrectAnswer() {
-    // answerReveal.innerText = 'Incorrect! Better luck next time';
-    score--; // Decrease the score by 1
-    updateScore(); // Update the displayed score
-}
-
-// Function to update the displayed score
-function updateScore() {
-    scoreElement.textContent = `Score: ${score}/${totalQuestions}`;
-}
+ {
+    questionUrl: 'assets/images/main-img/pionus.jpg',
+    a: 'Pionus',
+    b: 'Caique',
+    c: 'African Grey',
+    correct: this.a,
+    fact: '',
+ },
+ {
+    questionUrl: 'assets/images/main-img/gcc.jpg',
+    a: 'Green Cheek Conure',
+    b: 'Goffin Cockatoo',
+    c: 'Galah Cockatoo',
+    correct: this.a,
+    fact: '',
+ },
+ {
+    questionUrl: 'assets/images/main-img/caique.jpg',
+    a: 'Caique',
+    b: 'Lorikeet',
+    c: 'Cockatiel',
+    correct: this.a,
+    fact: '',
+ },
+ {
+    questionUrl: 'assets/images/main-img/irn.jpg',
+    a: 'Indian Ringneck',
+    b: 'Lovebird',
+    c: 'African Grey',
+    correct: this.a,
+    fact: '',
+ },
+{
+    questionUrl: 'assets/images/main-img/quaker.jpg',
+    a: 'Quaker',
+    b: 'Cockatiel',
+    c: 'Eclectus',
+    correct: this.a,
+    fact: '',
+ },
+ {
+    questionUrl: 'assets/images/main-img/lovebird.jpg',
+    a: 'Lovebird',
+    b: 'Scarlet Macaw',
+    c: 'Goffin Cockatoo',
+    correct: this.a,
+    fact: '',
+ },
+ {
+    questionUrl: 'assets/images/main-img/lori.jpg',
+    a: 'Lorikeet',
+    b: 'Indian Ringneck',
+    c: 'Green Cheek Conure',
+    correct: this.a,
+    fact: '',
+ },
+ {
+    questionUrl: 'assets/images/main-img/hyacinth.jpg',
+    a: 'Hyacinth Macaw',
+    b: 'Kakariki',
+    c: 'Yellow-Naped Amazon',
+    correct: this.a,
+    fact: '',
+ },
+ {
+    questionUrl: 'assets/images/main-img/galah.jpg',
+    a: 'Galah Cockatoo',
+    b: "Meyer's",
+    c: 'Quaker',
+    correct: this.a,
+    fact: '',
+ },
+{
+    questionUrl: 'assets/images/main-img/senegal.jpg',
+    a: 'Senegal',
+    b: 'Green Cheek Conure',
+    c: 'Eclectus',
+    fact: '',
+ },
+ {
+    questionUrl: 'assets/images/main-img/burrowing.jpg',
+    a: 'Burrowing',
+    b: 'Senegal',
+    c: 'Lorikeet',
+    correct: this.a,
+    fact: '',
+ },
+ {
+    questionUrl: 'assets/images/main-img/meyers.jpg',
+    a: "Meyer's",
+    b: 'Burrowing',
+    c: 'Goffin Cockatoo',
+    correct: this.a,
+    fact: '',
+ },
+ {
+    questionUrl: 'assets/images/main-img/kakariki.j
