b3b726aa7cf572042a1dd78c199ed37b180af50e
lrchnnng <laurachanning@hotmail.co.uk>
2023-10-24 15:48:39 +0100

    added extra message for scoreMessage

--- assets/js/score.js
+++ assets/js/score.js
@@ -18,15 +18,17 @@ const finalScoreValue = document.getElementById('f-score');
 
 function finalScoreMessage(){
    finalScoreValue.innerText = finalScore;
-
-   if (finalScore <= 4 ){
+   if (finalScore < 1) {
+      scoreMessage.innerText = "How did you get less than 1?!"
+      console.log('less than 1')
+   } else if (finalScore <= 4 ){
       scoreMessage.innerText = "Sorry! Better luck next time!"
-      console.log('')
+      console.log('less than 5')
    } else if (finalScore === 5) {
       scoreMessage.innerText = "Half way there!"
       console.log('score is 5')
    } else if (finalScore <= 9) {
-      scoreMessage.innerHTML = 'You did great!'
+      scoreMessage.innerHTML = "You did great!"
       console.log('score is 9 or under')
    }
    else if (finalScore === 10){
