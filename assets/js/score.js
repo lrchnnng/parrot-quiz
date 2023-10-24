478f56ce2b4e41498d1f7632bc47aaae1085ffaf
lrchnnng <laurachanning@hotmail.co.uk>
2023-10-24 15:42:36 +0100

    added a message depending on user score

--- assets/js/score.js
+++ assets/js/score.js
@@ -2,27 +2,35 @@
 const urlParams = new URLSearchParams(window.location.search);
 
 // Get the 'score' parameter from the URL
-const finalScore = urlParams.get('score');
+const finalScoreStr = urlParams.get('score');
+const finalScore = parseInt(finalScoreStr);
 
 // Use the score as needed
 console.log(`Final Score: ${finalScore}`);
 
-/* $(document).ready(function() {
+$(document).ready(function(finalScore) {
     finalScoreMessage();
     console.log("ready!");
  });
 
 const scoreMessage = document.getElementById('score-message');
-const finalScore = document.getElementById('f-score');
+const finalScoreValue = document.getElementById('f-score');
 
 function finalScoreMessage(){
-   if (score < 5){
-      console.log('under 5')
-   } else if (score < 10){
-      console.log('under 10')
-   } else if (score < 20){
-      console.log('under 20')
-   } else if (score === 20){
-      console.log('full marks')
+   finalScoreValue.innerText = finalScore;
+
+   if (finalScore <= 4 ){
+      scoreMessage.innerText = "Sorry! Better luck next time!"
+      console.log('')
+   } else if (finalScore === 5) {
+      scoreMessage.innerText = "Half way there!"
+      console.log('score is 5')
+   } else if (finalScore <= 9) {
+      scoreMessage.innerHTML = 'You did great!'
+      console.log('score is 9 or under')
    }
-} */
+   else if (finalScore === 10){
+      scoreMessage.innerText = "TOP MARKS!"
+      console.log('under 10')
+   }
+}
